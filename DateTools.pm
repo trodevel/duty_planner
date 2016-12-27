377a469eeefb9ca26f67083d3ad01032ee9c6f80
trodevel <trodevel@gmail.com>
2016-12-27 23:36:46 +0100

    bugfix: get_sunday_of_week() returned wrong day

--- DateTools.pm
+++ DateTools.pm
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
 
+# 1.1 - 16c27 - bugfix: get_sunday_of_week() returned wrong day
+
 package DateTools;
 
 use Date::Calc qw(:all);
@@ -17,7 +19,7 @@ sub get_sunday_of_week
 
     my( $year2, $month2, $day2 ) = get_monday_of_week( $week, $year );
 
-    return Add_Delta_Days( $year2, $month2, $day2, 7 );
+    return Add_Delta_Days( $year2, $month2, $day2, 6 );
 }
 
 1;
